29b6e0cb4d72985a23c9eac6dda44a9e4b8f6f94
Leydis <leydisfh@gmail.com>
2023-05-18 02:16:17 -0500

    Encriptado y desencriptado listo

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let entradaTexto = document.querySelector("#entrada_texto")
 let btnEncriptar = document.querySelector(`#btn_encriptar`);
 // Variable del campo de respuesta
 let respuesta = document.querySelector("#respuesta");
+// Variable del boton desencriptar
+let btnDesencriptar = document.querySelector("#btn_desencriptar");
 
 
 /*
@@ -17,9 +19,22 @@ La letra "u" es convertida para "ufat"*/
 btnEncriptar.addEventListener("click", encriptando);
 
 function encriptando(){
-    let encriptandoTexto = encriptar(entradaTexto.value);
+
+
+   /* let encriptandoTexto = encriptar(entradaTexto.value);
+    respuesta.textContent = encriptandoTexto;
+    entradaTexto.value = "";*/
+    let textoEntrada = entradaTexto.value;
+  
+   // Validación de letras minúsculas sin caracteres especiales ni acentos
+  if (/^[a-z\s]*$/.test(textoEntrada)) {
+    let encriptandoTexto = encriptar(textoEntrada);
     respuesta.textContent = encriptandoTexto;
     entradaTexto.value = "";
+  } else {
+    alert("El texto solo debe contener letras minúsculas sin caracteres especiales ni acentos.");
+  }
+
 }
 
 
@@ -32,7 +47,7 @@ function encriptar (stringEncriptado){
 //Condicional para reemplazar las letras
 for(let i = 0; i< codigoArray.length; i++ ){
     if(stringEncriptado.includes(codigoArray[i][0])){
-        stringEncriptado = stringEncriptado.replaceAll(codigoArray[i][0], codigoArray[i[1]])
+        stringEncriptado = stringEncriptado.replaceAll(codigoArray[i][0], codigoArray[i][1])
     }
 }
 return stringEncriptado
@@ -41,4 +56,42 @@ return stringEncriptado
 
 
 
+
+btnDesencriptar.addEventListener("click", desencriptando);
+
+function desencriptando(){
+   /* let encriptandoTexto = desencriptar(entradaTexto.value);
+    respuesta.textContent = encriptandoTexto;
+    entradaTexto.value = "";*/
+
+    let textoEntrada = entradaTexto.value;
+  
+   // Validación de letras minúsculas sin caracteres especiales ni acentos
+   if (/^[a-z\s]*$/.test(textoEntrada)) {
+    let desencriptandoTexto = desencriptar(textoEntrada);
+    respuesta.textContent = desencriptandoTexto;
+    entradaTexto.value = "";
+  } else {
+    alert("El texto solo debe contener letras minúsculas sin caracteres especiales ni acentos.");
+  }
+}
+
+
+
+
+//Funcion para desencriptar
+
+function desencriptar(stringDesencriptado){
+   
+    let codigoArray = [["e","enter"], ["i","imes"],["a","ai"], ["0","ober"], ["u", "ufat"]];
+   
+    stringDesencriptado = stringDesencriptado.toLowerCase();
+
+for(let i = 0; i< codigoArray.length; i++ ){
+    if(stringDesencriptado.includes(codigoArray[i][1])){
+        stringDesencriptado = stringDesencriptado.replaceAll(codigoArray[i][1],codigoArray[i] [0] )
+    }
+}
+return stringDesencriptado
+}
 
