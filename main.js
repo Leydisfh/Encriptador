dd22839be4b8b1b9e367e0782c52190591803c86
Leydis <leydisfh@gmail.com>
2023-05-17 23:02:57 -0500

    iniciando funciones de js

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+
+//Accediendo al boton de encriptar
+let encriptar = document.querySelector(`#encriptar`);
+//Creando evento al boton encriptar
+encriptar.addEventListener("click", encripta);
+
+function  encripta  () {
+alert("funciona")
+}
